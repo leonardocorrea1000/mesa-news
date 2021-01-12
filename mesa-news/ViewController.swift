e794dd9f2d4f207e68521a3fa5972944180f59bf
Jose Correa <correa.jose@cwi.com.br>
2021-01-11 22:44:07 -0300

    Initial commit

---
+++ mesa-news/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mesa-news
+//
+//  Created by Jose Correa on 11/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
